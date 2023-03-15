31cf9784ddfd4ea6039b4faa1bfd9a7dac8335b8
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-03-15 16:37:39 +0100

    Adding script to task no.4

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const counterDisplay = document.querySelector("#value");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+let counter = 0;
+
+updateDisplay();
+
+const decrementClick = () => {
+  counter--;
+  updateDisplay();
+};
+
+const incrementClick = () => {
+  counter++;
+  updateDisplay();
+};
+
+function updateDisplay() {
+  counterDisplay.innerHTML = counter;
+}
+
+decrementBtn.addEventListener("click", decrementClick);
+incrementBtn.addEventListener("click", incrementClick);
