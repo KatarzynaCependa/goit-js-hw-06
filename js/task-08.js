bacc5126bfd147981cb59c91fc6b6f464fa94977
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-03-19 15:55:03 +0100

    Adding script to task no. 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const inputEl = document.querySelector(".login-form");
 
+const submitHandler = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Proszę uzupełnić wsystkie pola");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+};
+
+inputEl.addEventListener("submit", submitHandler);
