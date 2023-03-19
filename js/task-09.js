5883c431c905248b9d7e69455ab90312c96437b6
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-03-19 16:53:41 +0100

    Adding script to task no. 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const firstEl = document.firstElementChild;
+const bodyEl = firstEl.lastElementChild;
+const buttonEl = document.querySelector(".change-color");
+const paragraphEl = document.querySelector(".color");
+
+const clickHandler = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  paragraphEl.textContent = getRandomHexColor();
+};
+
+buttonEl.addEventListener("click", clickHandler);
