6fbad2c5759072fdd3a2e5fc619cbdd5962ca0db
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-03-21 19:31:42 +0100

    Adding script to task no. 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBox = document.getElementById("boxes");
+const inputEl = document.querySelector('input[type="number"]');
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const newElement = document.createElement("div");
+    newElement.style.width = 30 + i * 10 + "px";
+    newElement.style.height = 30 + i * 10 + "px";
+    newElement.style.backgroundColor = getRandomHexColor();
+    divBox.append(newElement);
+  }
+}
+
+function destroyBoxes() {
+  divBox.remove();
+}
+
+createBtn.addEventListener("click", () => createBoxes(inputEl.value));
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+  inputEl.value = "";
+});
